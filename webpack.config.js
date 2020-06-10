c16e337605b32e1daae05df595148485338ff50f
Olga <vesnushka.ol@mail.ru>
2020-06-10 18:04:38 +0200

    Установит React и создаст первые компоненты

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,9 @@ module.exports = {
   },
   devServer: {
     contentBase: path.join(__dirname, `public`),
-    open: false,
+    open: true,
     port: 1337,
+    inline: true,
   },
   module: {
     rules: [
