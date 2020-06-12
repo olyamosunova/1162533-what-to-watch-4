7e35c4afae9793ce638d90da96ff575816ea912c
Olga <vesnushka.ol@mail.ru>
2020-06-12 15:24:07 +0200

    Напишет код для валидации всех props

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import App from "./components/app/app.jsx";
 const MovieDescription = {
   TITLE: `The Grand Budapest Hotel`,
   GENRE: `Drama`,
-  RELEASE_DATE: `2014`,
+  RELEASE_DATE: 2014,
 };
+
+const MOVIES_TITLES = [`Fantastic Beasts`, `Bohemian Rhapsody`, `Macbeth`];
 
 ReactDOM.render(
     <App title={MovieDescription.TITLE}
       genre={MovieDescription.GENRE}
-      releaseDate={MovieDescription.RELEASE_DATE} />,
+      releaseDate={MovieDescription.RELEASE_DATE}
+      moviesTitles={MOVIES_TITLES} />,
     document.querySelector(`#root`)
 );
